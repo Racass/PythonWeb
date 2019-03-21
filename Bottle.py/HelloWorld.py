5f1538e067540dcd474366e4baa7acfc65c575c5
Racass <31106782+Racass@users.noreply.github.com>
2019-03-21 11:49:42 -0300

    added Django as Djangeler

--- Bottle.py/HelloWorld.py
+++ Bottle.py/HelloWorld.py
@@ -1,6 +1,17 @@
 from bottle import Bottle, run, static_file, get, post, request
-from MySQL.Clientes import Clientes
+
 import os
+import pip #To check if mysql is installed
+
+def checkMySQL():
+    try:
+        from MySQL.Clientes import Clientes
+    except ImportError:
+        print("mySQL not installed, so will not work")
+        return False
+    return True
+
+mySQLImported = checkMySQL() # So I know in runtime if I have MySQL
 
 app = Bottle()
 
@@ -22,11 +33,14 @@ def do_form():
     nome = request.forms.get("nome")
     fone = request.forms.get("fone")
     email = request.forms.get("email")
-    cli = Clientes()
-    cli.nome = nome
-    cli.fone = fone
-    cli.email = email
-    cli.Sync()
-    return "nome: " + nome + " fone: " + fone + " email: " + email
+    if(mySQLImported):
+        cli = Clientes()
+        cli.nome = nome
+        cli.fone = fone
+        cli.email = email
+        cli.Sync()
+        return "nome: " + nome + " fone: " + fone + " email: " + email
+    else:
+        return "MySQL não importado." + "nome: " + nome + " fone: " + fone + " email: " + email
 
 run(app, host='localhost', port=8080)
